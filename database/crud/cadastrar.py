38e6a07bd635859c473e45a2592d87e7e0d9a0ff
erik-nathan <eriknathan.contato@gmail.com>
2023-04-17 19:19:57 -0300

    :sparkles: feat: Criando a função da cadastrar do Crud

---
+++ database/crud/cadastrar.py
@@ -0,0 +1,10 @@
+from database.database import connection
+
+
+def cadastrar_pessoa(nome, idade):
+    """Função para cadastrar uma pessoa no banco de dados"""
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO pessoas (nome, idade) VALUES (%s, %s)", (nome, idade))
+    connection.commit()
+    cursor.close()
+    
